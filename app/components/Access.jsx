f6e6f1db006b3681f16dcbb92e3ea6a98ecf7477
William Knight <kniwilliam@gmail.com>
2016-09-12 09:44:25 -0700

    Add input and flat button to access

--- app/components/Access.jsx
+++ app/components/Access.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 const Access = React.createClass({
   render() {
 
+    const inputStyle = {
+      borderColor: '#F4AF1D'
+    };
+
     return <div className="content-container">
-      <h1 className="main-header">Enter the unique code to access to access your KJs song list.
+      <h1 className="main-header">Enter the unique code to access to access your KJ's song list.
       </h1>
+      <TextField underlineFocusStyle={inputStyle}/>
+      <FlatButton label="View songs" />
     </div>
   }
 })
