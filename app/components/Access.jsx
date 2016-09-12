f638e179f917c99b4ba57d21976b45fdc173e09f
William Knight <kniwilliam@gmail.com>
2016-09-12 10:36:34 -0700

    Close #10 Create access component

--- app/components/Access.jsx
+++ app/components/Access.jsx
@@ -10,10 +10,15 @@ const Access = React.createClass({
     };
 
     return <div className="content-container">
-      <h1 className="main-header">Enter the unique code to access to access your KJ's song list.
+      <h1 className="main-header">Enter the unique code to access your KJ's song list.
       </h1>
-      <TextField underlineFocusStyle={inputStyle}/>
-      <FlatButton label="View songs" />
+      <div>
+        <TextField className='song-input' underlineFocusStyle={inputStyle}/>
+        <FlatButton
+          className='flat-btn'
+          label="View songs"
+          />
+      </div>
     </div>
   }
 })
