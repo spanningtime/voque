03f1c0e9a510ccc0ea322cee4e98476efe7877b2
William Knight <kniwilliam@gmail.com>
2016-09-12 13:39:50 -0700

    Add search icon

--- app/components/SongList.jsx
+++ app/components/SongList.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
 
 
 const SongList = React.createClass({
+
   render() {
+    const inputStyle = {
+      borderColor: '#F4AF1D'
+    };
+
     return <div className="content-container">
-      <h1 className="main-header">
-        Donnie's Song List
-      </h1>
+      <div className="header-container">
+        <h1 className="kj-name">
+          Donnie's Song List
+        </h1>
+        <div>
+        <TextField className='song-search' underlineFocusStyle={inputStyle}
+        />
+        <img className="search-icon"src={'./images/search.svg'} />
+        </div>
+      </div>
       <div className="songs-container">
-        
+
       </div>
     </div>
   }
