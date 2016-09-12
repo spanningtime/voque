f09fe6c3410ce4e36b2f84efb18b3b6c0851ae7b
William Knight <kniwilliam@gmail.com>
2016-09-12 11:57:28 -0700

    Add responsive div for songs background

--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -3,6 +3,7 @@ import App from 'components/app';
 import Landing from 'components/Landing';
 import React from 'react';
 import Access from 'components/Access';
+import SongList from 'components/SongList';
 
 const Routes = React.createClass({
   render() {
@@ -10,6 +11,7 @@ const Routes = React.createClass({
       <Route component={App} path="/" >
         <IndexRoute component={Landing} />
         <Route component={Access} path="/access" />
+        <Route component={SongList} path="/songlist" />
       </Route>
     </Router>;
   }
---
+++ app/components/SongList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const SongList = React.createClass({
+  render() {
+    return <div className="content-container">
+      <h1 className="main-header">
+        Donnie's Song List
+      </h1>
+      <div className="songs-container">
+        
+      </div>
+    </div>
+  }
+})
+
+export default SongList;
