4c8d4728c835049822497ce7b170675ff2e3f822
William Knight <kniwilliam@gmail.com>
2016-09-21 21:15:38 -0700

    Make functional back button for mobile thanks component

--- app/components/Thanks.jsx
+++ app/components/Thanks.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router';
+
 
 // EXAMPLE API call for track id
 //'http://api.musixmatch.com/ws/1.1/track.search?apikey=14685231d67e7ec9fe1bc89da7b6105b&q_artist=queen&q_track=we%20are%20the%20champions&format=json&page_size=1&f_has_lyrics=1'
@@ -7,10 +9,21 @@ import React from 'react';
 //'http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=14685231d67e7ec9fe1bc89da7b6105b&track_id=85217814'
 
 const Thanks = React.createClass({
+
+  goBack() {
+    this.props.router.goBack();
+  },
+
   render() {
 
     return <div className="content-container">
       <div className="thanks-container">
+      <div className="back-container">
+        <span className="back-btn"
+          onTouchTap={this.goBack}>
+          <img className="back-arrow" src={'./images/arrow.svg'} />
+        </span>
+      </div>
         <h1 className="main-header">
           Thanks for the request!
         </h1>
@@ -28,4 +41,4 @@ const Thanks = React.createClass({
   }
 })
 
-export default Thanks;
+export default withRouter(Thanks);
