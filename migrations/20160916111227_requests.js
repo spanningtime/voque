85813b0687945dc678aedd20140f89f1530f2fef
William Knight <kniwilliam@gmail.com>
2016-10-04 11:59:39 -0700

    Correct requests foreign keys

--- migrations/20160916111227_requests.js
+++ migrations/20160916111227_requests.js
@@ -4,18 +4,26 @@ exports.up = function(knex) {
   return knex.schema.createTable('requests', (table) => {
     table.increments();
     table.integer('admin_id')
+      .notNullable()
       .references('id')
       .inTable('users')
+      .onDelete('CASCADE')
+      .index();
     table.integer('singer_id')
+      .notNullable()
       .references('id')
       .inTable('users')
+      .onDelete('CASCADE')
+      .index();
     table.integer('song_id')
+      .notNullable()
       .references('id')
       .inTable('songs')
-      .notNullable();
+      .onDelete('CASCADE')
+      .index();
     table.string('artist_name');
-    table.string('song_title')
-    table.timestamps(true, true)
+    table.string('song_title');
+    table.timestamps(true, true);
   })
 };
 
