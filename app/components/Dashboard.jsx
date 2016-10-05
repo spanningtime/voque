51e8d3957b2bba49da4c3a1569bfa912690c1bf9
William Knight <kniwilliam@gmail.com>
2016-10-04 21:17:02 -0700

    Lint Dashboard.jsx

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import { withRouter } from 'react-router';
+import React from 'react';
 import Toggle from 'material-ui/Toggle';
+import { withRouter } from 'react-router';
 
 const Dashboard = React.createClass({
 
@@ -16,12 +16,12 @@ const Dashboard = React.createClass({
 
   render() {
     const styleToggle = {
-      marginBottom: 16,
+      marginBottom: 16
     };
 
     const styleIcon = {
       width: '50px',
-      color: '#f4af1d',
+      color: '#f4af1d'
     };
 
     const styleH1 = {
@@ -29,24 +29,31 @@ const Dashboard = React.createClass({
     };
 
     return <div className="content-container">
-      <h1 className="main-header"
-          style={styleH1}>Dashboard</h1>
+      <h1
+        className="main-header"
+        style={styleH1}
+      >
+        Dashboard
+      </h1>
       <div>
         <Toggle
-          label="Accept Requests?"
-          style={styleToggle}
           iconStyle={styleIcon}
-          toggled={this.props.accept}
+          label="Accept Requests?"
           onToggle={this.handleChange}
+          style={styleToggle}
+          toggled={this.props.accept}
         />
       </div>
 
       <RaisedButton
+        label="View Requests"
         onTouchTap={this.handleTouchTap}
-        label="View Requests"/>
+      />
 
+      {/* eslint-disable max-len */}
       <h5 id="current-code">Current code: <span id="code-name">{this.props.user.code}</span></h5>
-    </div>
+      {/* eslint-enable max-len*/}
+    </div>;
   }
 });
 
