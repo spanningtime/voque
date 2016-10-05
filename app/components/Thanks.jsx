4948d9894f60476579a6109b0ffa65b130025770
William Knight <kniwilliam@gmail.com>
2016-10-04 22:24:59 -0700

    Lint Thanks.jsx

--- app/components/Thanks.jsx
+++ app/components/Thanks.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router';
-
-
-// EXAMPLE API call for track id
-//'http://api.musixmatch.com/ws/1.1/track.search?apikey=14685231d67e7ec9fe1bc89da7b6105b&q_artist=queen&q_track=we%20are%20the%20champions&format=json&page_size=1&f_has_lyrics=1'
-
-//
-//'http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=14685231d67e7ec9fe1bc89da7b6105b&track_id=85217814'
+import { withRouter } from 'react-router';
 
 const Thanks = React.createClass({
-
   goBack() {
     this.props.router.goBack();
   },
 
   render() {
-
     return <div className="content-container">
       <div className="thanks-container">
-      <div className="back-container">
-        <span className="back-btn"
-          onTouchTap={this.goBack}>
-          <img className="back-arrow" src={'./images/arrow.svg'} />
-        </span>
-      </div>
+        <div className="back-container">
+          <span
+            className="back-btn"
+            onTouchTap={this.goBack}
+          >
+            <img className="back-arrow" src={'./images/arrow.svg'} />
+          </span>
+        </div>
         <h1 className="main-header">
           Thanks for the request!
         </h1>
         <h2 className="second-header">
-            While you wait, take a look at the lyrics for <br/>
-             <span className="lyrics-title">{this.props.requestedSong.songTitle}</span>
+          While you wait, take a look at the lyrics for <br />
+          <span className="lyrics-title">{this.props.requestedSong.songTitle}
+          </span>
         </h2>
       </div>
       <div className="lyrics-container">
@@ -37,8 +31,8 @@ const Thanks = React.createClass({
           {this.props.lyrics}
         </h6>
       </div>
-    </div>
+    </div>;
   }
-})
+});
 
 export default withRouter(Thanks);
