5231e892bdb891bd12f17eaf3616deeb0eba4ba9
William Knight <kniwilliam@gmail.com>
2016-10-24 20:44:21 -0700

    Add logic to prevent duplicate songs to be posted to db

--- routes/songs.js
+++ routes/songs.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 const boom = require('boom');
 const bcrypt = require('bcrypt-as-promised');
-const { camelizeKeys, decamelizeKeys} = require('humps');
+const { camelizeKeys, decamelizeKeys } = require('humps');
 // const ev = require('express-validation');
 // const validations = require('../validations/users');
 const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const multer = require('multer');
-const upload = multer({inMemory: true});
+const upload = multer({ inMemory: true });
 const parseString = require('xml2js').parseString;
 
 router.get('/api/songs/:adminId', (req, res, next) => {
@@ -25,6 +25,7 @@ router.get('/api/songs/:adminId', (req, res, next) => {
         return next();
       }
       const song = camelizeKeys(rows);
+
       res.send(song);
     })
     .catch((err) => {
@@ -36,24 +37,38 @@ router.post('/upload/songs/:adminId', upload.single('songlist'), (req, res, next
   const adminId = Number.parseInt(req.params.adminId);
   const buf = req.file.buffer;
   const str = buf.toString('utf8');
-  let tracksArray = [];
-
+  const tracksArray = [];
 
   knex('songs')
-    .then(() => {
+    .where('admin_id', adminId)
+    .then((exists) => {
       parseString(str, (err, result) => {
         const tracks = result.plist.dict[0].dict[0].dict;
+
         tracks.map((track) => {
-          const trackObj = {}
+          const trackObj = {};
 
           trackObj.adminId = adminId;
           trackObj.songTitle = track.string[0];
           trackObj.artistName = track.string[1];
-          tracksArray.push(trackObj);
+
+          for (let x = 0; x < exists.length; x++) {
+            if (
+              exists[x].song_title === trackObj.songTitle
+              &&
+              exists[x].artist_name === trackObj.artistName
+            ) {
+              return false;
+            }
+            else if (x === exists.length - 1) {
+              tracksArray.push(trackObj);
+            }
+          }
         });
       });
-      const rows = decamelizeKeys(tracksArray)
-      console.log(rows)
+
+      const rows = decamelizeKeys(tracksArray);
+
       return knex('songs').insert(rows, '*');
     })
     .catch((err) => {
