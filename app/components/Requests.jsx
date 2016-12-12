fed6a34222e4c64553a6552e7deddb5616f4d663
William Knight <kniwilliam@gmail.com>
2016-12-12 13:29:56 -0800

    Remove ReactCSSTransition & add animation for removing request

--- app/components/Requests.jsx
+++ app/components/Requests.jsx
@@ -1,19 +1,21 @@
 import axios from 'axios';
 import React from 'react';
 import weakKey from 'weak-key';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 const Requests = React.createClass({
 
   getInitialState() {
     return {
-      displayAnimation: 'none'
+      slide: false,
+      requestToDelete: null
     }
   },
 
+  componentDidMount() {
+    console.log('did mount');
+  },
+
   removeRequest(request) {
-    console.log(request)
-    this.setState({ displayAnimation: 'requestFade 1s ease-in 1' })
     axios.delete(`/api/requests/${request.adminId}/${request.id}`)
     .then(() => {
       this.props.getRequests();
@@ -23,14 +25,17 @@ const Requests = React.createClass({
     });
   },
 
+  removeAnimation(request) {
+    this.setState({ slide: true, requestToDelete: request }, this.removeRequest(request));
+  },
 
   render() {
     const styleNoRequests = {
       display: this.props.noRequestsDisplay
     }
 
-    const styleRequestAnimation = {
-      animation: this.state.displayAnimation
+    const makeSlide = {
+      animation: "slide 0.5s ease-in forwards",
     }
 
     return <div className="content-container">
@@ -48,20 +53,15 @@ const Requests = React.createClass({
 
             return <li key={weakKey(request)}>
               <div className="song-request-container">
-                {/* <ReactCSSTransitionGroup>
-
-                </ReactCSSTransitionGroup> */}
                 <i
                   className="material-icons"
                   id="delete"
                   onTouchTap={(() =>
-                     this.removeRequest(request)).bind(this)}
-                >
+                     this.removeAnimation(request)).bind(this)}>
                   clear
                 </i>
-                <div className="container-for-border"
-                     style={styleRequestAnimation}
-                >
+                <div className={"container-for-border " + (this.state.slide ? "slide" : "")}
+                      style={request === this.state.requestToDelete ? makeSlide : null}>
                   <div className="request-item-container">
                     <span>{this.props.singerName}</span>
                   </div>
